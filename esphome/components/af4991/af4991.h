6db8609d0289ed7d6f7c1b3bd7098ba3405210d2
MrEditor97 <mr.samuel.hughes@gmail.com>
2021-12-02 14:44:37 +0000

    AF4991 Encoder breakout implementation

---
+++ esphome/components/af4991/af4991.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "esphome/core/hal.h"
+#include "esphome/core/component.h"
+#include "esphome/components/adafruit_seesaw/adafruit_seesaw.h"
+
+namespace esphome {
+namespace af4991 {
+
+class AF4991 : public Component, public adafruit_seesaw::AdafruitSeesaw {
+ public:
+  void setup() override;
+  void dump_config() override;
+
+  float get_setup_priority() const override { return setup_priority::DATA; }
+
+ protected:
+  uint32_t product_version_ = 0;
+  enum ErrorCode {
+    NONE = 0,
+    INCORRECT_FIRMWARE_DETECTED,
+  } error_code_{NONE};
+};
+
+}  // namespace af4991
+}  // namespace esphome
