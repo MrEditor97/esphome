b88687a50d7f98a6bf8d196e1843866ca4e05eca
MrEditor97 <dev@mreditor97.co.uk>
2023-12-17 21:38:50 +0000

    Removed `cv.Component`

--- esphome/components/cst820/touchscreen.py
+++ esphome/components/cst820/touchscreen.py
@@ -22,15 +22,12 @@ CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
             cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
             cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
         }
-    )
-    .extend(i2c.i2c_device_schema(0x15))
-    .extend(cv.COMPONENT_SCHEMA)
+    ).extend(i2c.i2c_device_schema(0x15))
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     await touchscreen.register_touchscreen(var, config)
 
